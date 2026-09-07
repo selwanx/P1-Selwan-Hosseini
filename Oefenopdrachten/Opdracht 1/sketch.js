a0375cb9fcb49048b682349538d1c66e164c87d5
selwan <selwanjuni@gmail.com>
2026-09-07 12:18:55 +0200

    Worked on opdracht 1 - 2, not finished with 2 yet.

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(255, 255, 250)
+  stroke(10, 0, 0)
+  strokeWeight(3)
+  rect(300, 300, 500, 95)
+    fill(250, 255, 0)
+  rect(280, -1, 150, 150)
+      fill(100, 100, 255)
+  line(150, 130, 700, 130)
+  rect(0, 200, 150, -500)
+      fill(255, 0, 0)
+  rect(150, 200, 150, 150)
+      fill(255, 0, 0)
+  rect(150, 200, 150, 150)
+      fill(255, 0, 0)
+  rect(150, 200, 150, 150)
+      fill(255, 0, 0)
+  rect(150, 200, 150, 150)
+
 }
--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -4,4 +4,20 @@ function setup() {
 
 function draw() {
   background(220);
+let score = 100;
+score = 1;
+  let x = 100
+  text(x, 20, 20)
+  let greeting = "Hello World!"
+  text(greeting, 20, 60)
+  let a = 20
+  let b = 10
+  let optellen = a + b
+  let aftrekken = a - b
+  let vermenigvuldigen = a * b
+  let delen = a / b
+  text("optellen: " +  optellen, 20, 80)
+  text("optellen: " +  optellen, 20, 100)
+  text("optellen: " +  optellen, 20, 120)
+  text("optellen: " +  optellen, 20, 140)
 }
