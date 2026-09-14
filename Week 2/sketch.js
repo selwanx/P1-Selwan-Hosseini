8a1103236970276c937ae7b7cad229a2390e362a
selwan <selwanjuni@gmail.com>
2026-09-14 14:16:45 +0200

    added movement and functionality to the traffic light

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,4 +1,13 @@
 let licht = 0;
+let zon = 0;
+let cloud1 = 900;
+let cloud2 = 900;
+let sunsize1 = 120;
+let sunsize2 = 100;
+let treemove1 = 0;
+let treemove2 = 0;
+let treemove3 = 0;
+let treemove4 = 0;
 
 function setup() {
   createCanvas(800, 600);
@@ -8,32 +17,37 @@ function draw() {
   background("#7dafff");
 
 //zon
-   fill(254, 244, 113, 127)
-   circle(475, 85, 120)
   fill(254, 244, 113, 127)
-   circle(475, 85, 100)
+  sunsize1 = 100 + Math.sin(frameCount * 0.05) * 20
+  sunsize2 = 80 + Math.sin(frameCount * 0.05) * 20
+   circle(zon + -120, 85, sunsize1)
+  fill(254, 244, 113, 127)
+   circle(zon + -120 - 0, 85, sunsize2)
   fill("#fdff94")
-   circle(475, 85, 70)
-  
+   circle(zon + -120, 85, 70)
+zon = frameCount % 1040
+ 
   //clouds
   fill("#e0e0e0")
-  circle(130, 97, 50)
-  circle(175, 100, 70)
-  circle(220, 110, 50)
+  circle(830 - cloud1, 97, 50)
+  circle(875 - cloud1, 100, 70)
+  circle(920 - cloud1, 110, 50)
     fill("#ffffff")
-  circle(130, 107, 50)
-  circle(175, 110, 70)
-  circle(220, 120, 50)
-
+  circle(830 - cloud1, 107, 50)
+  circle(875 - cloud1, 110, 70)
+  circle(920 - cloud1, 120, 50)
+cloud1 = frameCount % 950;
+  
     fill("#e0e0e0")
-  circle(600, 147, 50)
-  circle(645, 150, 70)
-  circle(690, 160, 50)
+  circle(1300 - cloud2, 147, 50)
+  circle(1345 - cloud2, 150, 70)
+  circle(1390 - cloud2, 160, 50)
     fill("#ffffff")
-  circle(600, 157, 50)
-  circle(645, 160, 70)
-  circle(690, 170, 50)
-    
+  circle(1300 - cloud2, 157, 50)
+  circle(1345 - cloud2, 160, 70)
+  circle(1390 - cloud2, 170, 50)
+cloud2 = frameCount % 1850;
+  
   //bergen
   stroke(1)
   fill("#3d3d3d")
@@ -70,61 +84,79 @@ function draw() {
   rect(655, 435, 7, 100, 10)
 
   if (licht == 0) {
-  fill("#420000")
+  fill("#ff0000")
+  } else {
+    fill("#570000")
+  }
   circle(658, 370, 25)
-  fill("#3d1f00")
-  circle(658, 400, 25)
-  fill("#00ff01")
-  circle(658, 430, 25)
 
+   if (licht == 2) {
+  fill("#ff6600")
+  } else {
+    fill("#753f00")
+  }
+  circle(658, 400, 25)
+
+    if (licht == 1) {
+  fill("#41d900")
+  } else {
+    fill("#00280c")
+  }
+  circle(658, 430, 25)
+  
+  
   //bomen
   fill("#523320")
   rect(80, 408, 17.5, 120)
   fill("#005e00")
-  circle(80, 365, 85)
+  circle(treemove1 + 80, 365, 85)
    fill("#055705")
-  circle(75, 375, 85)
+  circle(treemove2 + 75, 375, 85)
    fill("#004d00")
-  circle(83, 385, 85)
+  circle(treemove3 + 83, 385, 85)
    fill("#004700")
-  circle(90, 380, 85)
-
+  circle(treemove4 + 90, 380, 85)
+    
   //boom 2
    fill("#523320")
   rect(210, 408, 17.5, 120)
   fill("#005e00")
-  circle(210, 365, 85)
+  circle(treemove1 + 210, 365, 85)
    fill("#055705")
-  circle(205, 375, 85)
+  circle(treemove2 + 205, 375, 85)
    fill("#004d00")
-  circle(213, 385, 85)
+  circle(treemove3 + 213, 385, 85)
    fill("#004700")
-  circle(220, 380, 85)
-
+  circle(treemove4 + 220, 380, 85)
+    
    //boom 3
    fill("#523320")
   rect(460, 408, 17.5, 120)
   fill("#005e00")
-  circle(460, 365, 85)
+  circle(treemove1 + 460, 365, 85)
    fill("#055705")
-  circle(455, 375, 85)
+  circle(treemove2 + 455, 375, 85)
    fill("#004d00")
-  circle(463, 385, 85)
+  circle(treemove3 + 463, 385, 85)
    fill("#004700")
-  circle(470, 380, 85)
-
+  circle(treemove4 + 470, 380, 85)
+    
    //boom 4
    fill("#523320")
   rect(330, 525, 17.5, 120)
   fill("#005e00")
-  circle(330, 482, 85)
+  circle(treemove1 + 330, 482, 85)
    fill("#055705")
-  circle(325, 492, 85)
+  circle(treemove2 + 325, 492, 85)
    fill("#004d00")
-  circle(333, 502, 85)
+  circle(treemove3 + 333, 502, 85)
    fill("#004700")
-  circle(340, 497, 85)
-
+  circle(treemove4 + 340, 497, 85)
+  treemove1 = 0 + Math.sin(frameCount * 0.075) * 5
+  treemove2 = 0 + Math.sin(frameCount * 0.05) * 4
+  treemove3 = 0 + Math.sin(frameCount * 0.1) * 2
+  treemove4 = 0 + Math.sin(frameCount * 0.05) * 3
+    
   //auto 1
   fill("#5c21d1")
   rect(80, 490, 100, 75)
@@ -132,6 +164,14 @@ function draw() {
   fill("#1f1f1f")
   circle(100, 570, 40)
   circle(190, 570, 40)
-
   
+}
+
+function keyPressed () {
+  if (keyCode == ENTER) {
+    licht = licht + 1
+    if (licht > 2) {
+      licht = 0
+    }
+    }
 }
