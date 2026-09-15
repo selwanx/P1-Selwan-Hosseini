3e1677ad88efe896d2d61d3ddbaf790c92565392
selwan <selwanjuni@gmail.com>
2026-09-15 12:08:35 +0200

    1.0

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,47 @@
+let score = 0;
+let happy;
+let smile;
+let meh;
+let mad;
+
+
+function preload() {
+  happy = loadImage("happy.png");
+  smile = loadImage("smile.png");
+  meh = loadImage("meh.png");
+  mad = loadImage("mad.png");
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
   background(220);
+
+  if (score >= 90) {
+    fill(0, 255, 0);
+    text("Uitstekend!", 10, 100);
+    image(happy, 10, 120, 50, 50);
+  }   else if (score <= 89 && score >= 70) {
+    fill(255, 255, 0);
+    text("Goed Gedaan!", 10, 100);
+    image(smile, 10, 120, 50, 50);
+  }   else if (score <= 69 && score >= 50) {
+    fill('orange');
+    text("Voldoende", 10, 100); 
+    image(meh, 10, 120, 50, 50);
+  }  else if (score <= 49) {
+    fill(255, 0, 0);
+    text("Onvoldoende", 10, 100);
+    image(mad, 10, 120, 50, 50);
 }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    score = random(0, 100); 
+  }
+}
+
--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,7 +1,30 @@
+let score = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+
+  fill(0);
+text("Houd B in om een blokje te laten verschijnen.", 20, 15);
+text("Druk op spatie om het getal op 0 te zetten.", 20, 150);
+text("Druk op enter om van rood -> groen -> oranje te gaan.", 20, 300);
+text("Beweeg de eightball met WASD of de pijltjestoetsen.", 500, 15);
+
+if (keyIsDown(66)) {
+  fill('white');
+  rect(35, 35, 60, 60);
+} 
+
+  text("Score: " + score, 50, 200);
+score = score + 1;
+if (keyIsDown(32)) {
+  score = 0;
 }
+if (score === 500) {
+  score = 0;
+}
+}
+
